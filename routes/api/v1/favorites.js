d2f35429aef885799cce1b5c5021031a58d8946f
Ryan Hantak <rhantak@gmail.com>
2020-02-03 16:18:48 -0700

    Add functionality in post favorite route to add favorite to database
    Co-Authored-By: Alice Post <ap2322@users.noreply.github.com>

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -5,14 +5,16 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 const fetch = require("node-fetch");
+const Favorite = require("../../../lib/models/favorite")
 
 async function fetchMusicData(info) {
   let url = `https://api.musixmatch.com/ws/1.1/track.search?q_track=${info["title"]}&q_artist=${info["artistName"]}&s_artist_rating=desc&s_track_rating=desc&page_size=1&apikey=${process.env.MM_KEY}`
   let data = await fetch(url)
     .then(response => response.json())
     .then(result => {
-      return result.message.body.track_list[0].track;
+        return result.message.body.track_list;
     })
+    .catch((error) => response.send(500).json({ error: error }))
     return data;
 }
 
@@ -28,12 +30,20 @@ router.post('/', (request, response) => {
   }
 
   let musicData = fetchMusicData(info)
-    .then(data => response.send(data))
-  // default no genre to unknown
-  // add fetched data to favorites table
-  // return success response
-  // set condition for returning 400 response
-  // catch 500
+    .then(data => {
+      if(data.length === 0) {
+        response.status(400).json({
+          "error": "Unable to create favorite.",
+          "detail": "No tracks were found from your search."
+        })
+      } else {
+        let newFavorite = new Favorite(data[0].track)
+
+        database('favorites').insert(newFavorite)
+          .then(response.status(201).send(newFavorite))
+      }
+    })
+    .catch((error) => response.status(500).json({error: error}))
 });
 
 module.exports = router;
