5ad0edbca18476aa6c3ef7e8d110e77cf415c118
Alice Post <alicep2322@gmail.com>
2020-02-11 14:39:17 -0700

    Add migration to add playlistFavorites resource to database
    Co-Authored-By: Ryan Hantak <rhantak@users.noreply.github.com>

---
+++ db/migrations/20200211143227_addPlaylistFavorites.js
@@ -0,0 +1,22 @@
+
+exports.up = function(knex) {
+  return Promise.all([
+    knex.schema.createTable('playlistFavorites', function(table) {
+      table.increments('id').primary();
+
+      table.integer('playlist_id').unsigned().notNullable();
+      table.foreign('playlist_id').references('id').inTable('playlists');
+
+      table.integer('favorite_id').unsigned().notNullable();
+      table.foreign('favorite_id').references('id').inTable('favorites');
+
+      table.timestamps(true, true)
+    })
+  ])
+};
+
+exports.down = function(knex) {
+  return Promise.all([
+    knex.schema.dropTable('playlistFavorites')
+  ]);
+};
